273c9d22b03ed88e10d484b5f44107f4736447ed
aravinthan98 <117471432+aravinthan98@users.noreply.github.com>
2024-02-28 02:33:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ const sections = document.querySelectorAll('.item-container');
 
 sections.forEach(section => {
     const bottomContent = section.querySelector('.bottom-content');
-    const checkbox=section.querySelector('#check');
-
+   
     section.addEventListener('mouseover', () => {
-        bottomContent.style.display = 'block';
-        checkbox.checked=true;
+        bottomContent.style.display = 'block';       
     });
 
     section.addEventListener('mouseout', () => {
         bottomContent.style.display = 'none';
-        checkbox.checked=false;
     });
 });
